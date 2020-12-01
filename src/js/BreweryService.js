82a663453ecacab195b1e8989c564cd38c03545d
Joey3001 <josephnero111@gmail.com>
2020-11-30 23:12:31 -0800

    Added boxes to enter address

--- src/js/BreweryService.js
+++ src/js/BreweryService.js
@@ -12,7 +12,7 @@ export class BreweryService {
     });
   }
   static addressCoords(street, city, state, zip) {
-    return fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=OR0fxFNZAFPABnEFJT0HECNIiDCGgu9l&street=${street}&city=${city}&state=${state}&postalCode=${zip}`)
+    return fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=mtwjNpdStw1KmG04UiK79YlvKt4W2PEO&street=${street}&city=${city}&state=${state}&postalCode=${zip}`)
     .then(function (response) {
       if (!response.ok) {
         throw Error(response.statusText);
@@ -25,28 +25,32 @@ export class BreweryService {
   }
 }
 
-  //   try {
-  //     const response = await fetch(`http://beermapping.com/webservice/locstate/${process.env.API_KEY}/${state}&s=json`);
-  //     if (!response.ok) {
-  //       throw Error(response.statusText);
-  //     }
-  //     return response.json();
-  //   } catch(error) {
-  //       return error.message;
-  //   }
-  //}
-  //static breweryCoord(ID) {
-  //   try {
-  //     const response = await fetch(`http://beermapping.com/webservice/locmap/${process.env.API_KEY}/${ID}&s=json`);
-  //     if (!response.ok) {
-  //       throw Error(response.statusText);
-  //     }
-  //     return response.json();
-  //   } catch(error) {
-  //       return error.message;
-  //   }
-  // }
-//}
+//     try {
+//       const response = await fetch(`http://beermapping.com/webservice/locstate/${process.env.API_KEY}/${state}&s=json`);
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+//       return response.json();
+//     } catch(error) {
+//         return error.message;
+//     }
+  
+//   static breweryCoord(ID) {
+//     try {
+//       const response = await fetch(`http://beermapping.com/webservice/locmap/${process.env.API_KEY}/${ID}&s=json`);
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+//       return response.json();
+//     } catch(error) {
+//         return error.message;
+//     }
+//   }
 
+// [[promiseStatus]]
 
+// [[PromiseResult]]
+// [array of shit you want]
 
+// let x = findbrewery(co);
+// console.log(x);
--- src/main.js
+++ src/main.js
@@ -3,38 +3,30 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { BreweryService } from './js/BreweryService.js';
+let z = [];
+let x = [];
 
 $(document).ready(function() {
-  $('#breweryInput').click(function() {
-    BreweryService.findBrewery('co') 
+  $('#breweryInput').click(async function() {
+    let street = $('#street').val();
+    let city = $('#city').val();
+    let state = $('#state').val();
+    let zip = $('#zip').val();
+    BreweryService.addressCoords(street, city, state, zip)
       .then(function(response) {
-        let z = [];
+        x = response
+      })
+    let promise = BreweryService.findBrewery('co') 
+      .then(function(response) {
         for (let i = 0; i < response.length ; i++) {
           BreweryService.addressCoords(response[i].street.replace(/ /g,"+"), response[i].city.replace(/ /g,"+"), 'Colorado', response[i].zip)
           .then(function(response) {
             z[i] = response.results[0].locations[0].displayLatLng;
           })
         }
-    });
+      });
+    await promise;
+    console.log(z);
+    console.log(x);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
