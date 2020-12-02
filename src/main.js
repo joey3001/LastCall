e508f88fb3da247eec6bed527bda27d7a39c1873
Joey3001 <josephnero111@gmail.com>
2020-12-01 22:58:09 -0800

    Fixed Async Bug

--- src/main.js
+++ src/main.js
@@ -3,23 +3,21 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { BreweryService } from './js/BreweryService.js';
-let breweryLatLng = [];
-let userAddressLatLng = [];
-let breweryListPerState = [];
-let breweryResult = [];
 
 async function trial(breweryListPerState, stateName2, userDist,selector) {
-  for (let i = 0; i < breweryListPerState.length;) {//TODO
-  let response3 = await BreweryService.addressCoords("", breweryListPerState[i].street.replace(/ /g,"+"), breweryListPerState[i].city.replace(/ /g,"+"), stateName2, breweryListPerState[i].zip); 
-  let nameDist = {
-    distance: null,
-    name: null,
-  }
-  breweryLatLng[i] = response3.results[0].locations[0].displayLatLng;
-  let dLng = (breweryLatLng[i].lng - userAddressLatLng.lng);
-  let dLat = (breweryLatLng[i].lat - userAddressLatLng.lat);
-  let distance = 69 * Math.sqrt(Math.pow(dLng, 2) + Math.pow(dLat, 2));
-  if (distance <= userDist) {
+  let breweryResult = [];
+  let breweryLatLng = [];
+  for (let i = 0; i < breweryListPerState.length; i++) {//TODO
+    let response3 = await BreweryService.addressCoords("", breweryListPerState[i].street.replace(/ /g,"+"), breweryListPerState[i].city.replace(/ /g,"+"), stateName2, breweryListPerState[i].zip); 
+    let nameDist = {
+      distance: null,
+      name: null,
+    }
+    breweryLatLng[i] = response3.results[0].locations[0].displayLatLng;
+    let dLng = (breweryLatLng[i].lng - userAddressLatLng.lng);
+    let dLat = (breweryLatLng[i].lat - userAddressLatLng.lat);
+    let distance = 69 * Math.sqrt(Math.pow(dLng, 2) + Math.pow(dLat, 2));
+    if (distance <= userDist) {
       nameDist.distance = distance;
       nameDist.name = breweryListPerState[i];
       breweryResult.push(nameDist);
@@ -27,21 +25,21 @@ async function trial(breweryListPerState, stateName2, userDist,selector) {
     breweryResult.sort(function(a,b) {
       return a.distance - b.distance;
     });
-    alert(i); 
-    alert(breweryResult.length); 
-    alert(breweryListPerState.length); 
-    if(breweryResult.length === breweryListPerState.length) {
-      for (let i = 0; i < 20 ; i++) { 
-        $(selector).append(breweryResult[i].name.name);
+    console.log(i);
+    console.log(breweryListPerState.length);
+    if(i === breweryListPerState.length-1) {
+      for (let j = 0; j < breweryResult.length ; j++) { 
+        $(selector).append('<li>' + breweryResult[j].name.name + '</li>');
       }
     }
-  } 
+  }
 }
-
 
 $(document).ready(function() {
   $('#breweryInput').submit(async function() {
     event.preventDefault();
+    let userAddressLatLng = [];
+    let breweryListPerState = [];
     let compass = $('#compass').val();
     let street = $('#street').val().replace(/ /g,"+");
     let city = $('#city').val().replace(/ /g,"+");
@@ -55,6 +53,6 @@ $(document).ready(function() {
     userAddressLatLng = response.results[0].locations[0].displayLatLng;
     let response2 = await BreweryService.findBrewery(stateAbv) 
     breweryListPerState = response2.filter(brewery => (brewery.status === "Brewpub" || brewery.status === "Brewery"));
-    await trial(breweryListPerState, stateName2, userDist, '#output');
+    trial(breweryListPerState, stateName2, userDist, '#output');
   });
 });
