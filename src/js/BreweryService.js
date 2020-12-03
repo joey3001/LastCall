61cacd62883754e98521701ab8a77ead88f4d511
Constantine <faustlarsen@Constantines-MacBook-Pro-2.local>
2020-12-03 11:04:40 -0700

    hide mp3 player

--- src/js/BreweryService.js
+++ src/js/BreweryService.js
@@ -1,7 +1,7 @@
 export class BreweryService {
   static async findBrewery(state) {
     try { 
-      const response = await fetch (`http://beermapping.com/webservice/locstate/9fc31f691baa30b0ee58bcc49747b840/${state}&s=json`);
+      const response = await fetch (`http://beermapping.com/webservice/locstate/${process.env.API_KEY}/${state}&s=json`);
       if (!response.ok) {
         throw Error(response.statusText); 
       }
@@ -14,7 +14,7 @@ export class BreweryService {
 
   static async addressCoords(compass, street, city, state, zip) {
     try { 
-      const response = await fetch (`http://www.mapquestapi.com/geocoding/v1/address?key=W93i9RAAUIOJySGNRJvUGtKJbIEdjTwN&street=${street}+${compass}&city=${city}&state=${state}&postalCode=${zip}`);
+      const response = await fetch (`http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.API_KEY2}&street=${street}+${compass}&city=${city}&state=${state}&postalCode=${zip}`);
       if (!response.ok) {
         throw Error(response.statusText); 
       }
