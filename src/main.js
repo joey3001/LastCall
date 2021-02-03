145ee1d14295e716fdc9291f4ceeea1475e3a6c9
Joey3001 <josephnero111@gmail.com>
2021-02-02 16:33:31 -0800

    Clean up uneccesary code.

--- src/main.js
+++ src/main.js
@@ -4,147 +4,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ApiCalls from "./js/ApiCalls.js";
 import BreweryFunctions from "./js/BreweryFunctions.js";
-import Beer from "../src/js/beerConstructor.js";
-import Beers from "../src/js/beers.js";
-import User from "../src/js/user.js";
 
-$(document).ready(function () {
-
-  //Logic for confirming you are 21 on the opening page
-  $(".btn-no").click(function () {
-    window.location.href = "http://google.com/";
-  });
-
-  //Logic for brewery finder
-  $("#breweryInput").submit(async function () {
-    event.preventDefault();
-    const street = $("#street").val().replace(/ /g, "+");
-    const city = $("#city").val().replace(/ /g, "+");
-
-    const stateArray = $("#state").val().split(", ");
-    const stateAbv = stateArray[0];
-    const stateName = stateArray[1].replace(/ /g, "+");
-
-    const zip = $("#zip").val().replace(/ /g, "+");
-
-    const searchRadius = parseInt($("#searchRadius").val());
-    const userAddressInfo = await ApiCalls.addressCoords(street, city, stateName, zip);
-    const userAddressLatLng = userAddressInfo.results[0].locations[0].displayLatLng;
-
-    const alcoholSalesListByState = await ApiCalls.findBrewery(stateAbv);
-    const breweryListByState = BreweryFunctions.breweryStateFilter(alcoholSalesListByState);
-    const filteredBreweriesByDistance = await BreweryFunctions.breweryDistanceFilter(breweryListByState, userAddressLatLng, stateName, searchRadius); 
-    const sortedBreweriesByDistance = BreweryFunctions.breweryDistanceSorter(filteredBreweriesByDistance); 
-
-    BreweryFunctions.breweryPost(sortedBreweriesByDistance, "#output"); 
-  });
-
-
-
-
-
-
-
-
-
-  //Logic for quiz appearence
-  $("#startQuiz").click(function () {
-    $("#titlePage").fadeOut();
-    nextQuestion();
-  });
-
-  const question = $(".form-group");
-  const nextButton = $("#next");
-  const backButton = $("#back");
-  const submitButton = $("#submit");
-  let step = 0;
-
-  function hideAll() {
-    submitButton.hide();
-    question.delay(400).fadeOut();
-    nextButton.delay(400).fadeOut();
-    backButton.delay(400).fadeOut();
-  }
-
-  function showButtons() {
-    nextButton.fadeIn();
-    backButton.fadeIn();
-  }
-
-  function showSubmitButton() {
-    submitButton.show();
-    //or can change to show questions if the user wants to review their answers
-    question.hide();
-    nextButton.hide();
-    backButton.hide();
-  }
-
-  function nextQuestion() {
-    if (step === 0) {
-      submitButton.hide();
-      question.hide();
-      backButton.hide();
-      nextButton.show();
-      $("div[value=1]").show();
-    } else if (step === 1) {
-      hideAll();
-      $("div[value=2]").delay(400).fadeIn(300);
-      showButtons();
-    } else if (step === 2) {
-      hideAll();
-      $("div[value=3]").delay(400).fadeIn(300);
-      showButtons();
-    } else if (step === 3) {
-      hideAll();
-      $("div[value=4]").delay(400).fadeIn(300);
-      showSubmitButton();
-    }
-  }
-
-  nextButton.click(function () {
-    step++;
-    nextQuestion();
-  });
-  backButton.click(function () {
-    step--;
-    nextQuestion();
-  });
-
-  //logic for submit button
-  $("#quiz").submit(function (event) {
-    event.preventDefault();
-    hideAll();
-    const IPA = new Beer(1, "IPA", 2, 4, 1, 100);
-    const Pilsner = new Beer(2, "Pilsner", 1, 3, 1, 200);
-    const Hefewiezen = new Beer(3, "Hefewiezen", 2, 1, 2, 300);
-    const PaleAle = new Beer(4, "Pale Ale", 3, 2, 2, 400);
-    const BrownAle = new Beer(5, "Brown Ale", 4, 3, 2, 500);
-    const Stout = new Beer(6, "Stout", 5, 2, 2, 600);
-    const SourBeer = new Beer(7, "Sour Beer", 7, 1, 2, 700);
-
-    let beers = new Beers();
-    beers.addBeers(IPA);
-    beers.addBeers(Pilsner);
-    beers.addBeers(Hefewiezen);
-    beers.addBeers(PaleAle);
-    beers.addBeers(BrownAle);
-    beers.addBeers(Stout);
-    beers.addBeers(SourBeer);
-
-
-    const userColor = parseInt($("input:radio[name=color]:checked").val());
-    const userIbu = parseInt($("input:radio[name=ibu]:checked").val());
-    const userClarity = parseInt($("input:radio[name=clarity]:checked").val());
-    const userFlavor = parseInt($("input:radio[name=flavor]:checked").val());
-
-    let user = new User();
-    user.showBeerResult(userColor, userIbu, userClarity, userFlavor, beers);
-    
-    if (user.beersLiked.length >0) {
-      $("ul#result").text(`You could try:`);
-      for (let i = 0; i < user.beersLiked.length; i++) {
-        $("ul#result").append(`<li>${user.beersLiked[i].name}</li>`);
-      }
-    } else $("#resultError").text('Sorry, we dont have beer to offer. Try again.');
-  });
+//Logic for confirming you are 21 on the opening page
+$(".btn-no").click(function () {
+  window.location.href = "http://google.com/";
 });
+
+//Logic for brewery finder
+$("#breweryInput").submit(async function () {
+  event.preventDefault();
+  const street = $("#street").val().replace(/ /g, "+");
+  const city = $("#city").val().replace(/ /g, "+");
+
+  const stateArray = $("#state").val().split(", ");
+  const stateAbv = stateArray[0];
+  const stateName = stateArray[1].replace(/ /g, "+");
+
+  const zip = $("#zip").val().replace(/ /g, "+");
+
+  const searchRadius = parseInt($("#searchRadius").val());
+  const userAddressInfo = await ApiCalls.addressCoords(street, city, stateName, zip);
+  const userAddressLatLng = userAddressInfo.results[0].locations[0].displayLatLng;
+
+  const alcoholSalesListByState = await ApiCalls.findBrewery(stateAbv);
+  const breweryListByState = BreweryFunctions.breweryStateFilter(alcoholSalesListByState);
+  const filteredBreweriesByDistance = await BreweryFunctions.breweryDistanceFilter(breweryListByState, userAddressLatLng, stateName, searchRadius); 
+  const sortedBreweriesByDistance = BreweryFunctions.breweryDistanceSorter(filteredBreweriesByDistance); 
+
+  BreweryFunctions.breweryPost(sortedBreweriesByDistance, "#output"); 
+});
+
